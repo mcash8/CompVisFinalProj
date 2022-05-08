dcd9af2943d1f9b84a14ec54eb43754ba04fd072
Martha Cash <63068410+mcash8@users.noreply.github.com>
2022-05-07 19:15:51 -0500

    Add files via upload

--- Eval_PSNR_SSIM.m
+++ Eval_PSNR_SSIM.m
@@ -1,13 +1,56 @@
+%ground truth results
+gt_dir = ('D:\SIDD\ValidationGtBlocksSrgb'); 
+load(gt_dir, "-mat", 'ValidationGtBlocksSrgb'); 
+
 %baseline results DIR 
-SiddDataDir = 'C:\Users\mcash8\Downloads\SIDD_Benchmark_Code_v1.1\SIDD_Benchmark_Code_v1.1\SIDD_Benchmark_Data\SIDD_Benchmark_Data'; 
-baseline_dir = fullfile(SiddDataDir, '..', 'Submit');
-%methods = {'gamma', 'clahe', 'histogramequalization', 'cbm3d'}; 
+baseline_dir = 'D:\SIDD\Results\'; 
 
-methods = {'gamma'};
+methods = {'gamma', 'clahe', 'histogramequalization'}; 
+
 nImages = 40; 
+nBlocks = 32;
+PSNRs = cell(nImages, nBlocks); %used in for loop
 
-%need ground truth images 
+% avg_psnr  = zeros(4, 40); 
+% j=1;
+% for method = methods
+%      Method = string(method); 
+%      baseline_file = strcat(Method, '.mat'); 
+%      
+%      %load .mat file -- results are in DenoisedBlocksSrgb variable
+%      %DenoisedBlockSrgb: 40x32 rows = benchmark images columns = patch 
+%      cd(baseline_dir)
+%      load(baseline_file);         
+%     for i = 1:nImages
+%         for b = 1 : nBlocks 
+% 
+%             fprintf('Calculating PSNR on sRGB image %02d, block %02d ... \n ', i, b);
+% 
+%             gt = double(squeeze(ValidationGtBlocksSrgb(i,b,:,:,:)));
+%             denoised = double(DenoisedBlocksSrgb{i,b}); 
+% 
+%             %calc PSNR
+%             PSNRs{i,b} = psnr(denoised, gt); 
+%         end 
+%         avg_psnr(j,i) = mean([PSNRs{i,:}]); %avg psnr for current image
+%         
+%     end 
+% 
+%     j = j+1;  %update counter for methods
+% 
+%     fprintf('done!\n')
+% end 
+% 
+% %save results 
+% filename = 'psnr_results.mat'; 
+% 
+% save(filename, "avg_psnr"); 
 
+%calculating mean SSIM
+
+avg_ssim = zeros(4,40); 
+j=1; 
+SSIMs = cell(nImages, nBlocks);
 
 for method = methods
      Method = string(method); 
@@ -17,16 +60,33 @@ for method = methods
      %DenoisedBlockSrgb: 40x32 rows = benchmark images columns = patch 
      cd(baseline_dir)
      load(baseline_file);         
-     
-     % block positions
-    tmp = load(fullfile('BenchmarkBlocks32.mat')); 
-    BenchmarkBlocks32 = tmp.BenchmarkBlocks32;
-    nBlocks = size(BenchmarkBlocks32, 1);
-    
     for i = 1:nImages
         for b = 1 : nBlocks 
-            
+
+            fprintf('Calculating SSIM on sRGB image %02d, block %02d ... \n ', i, b);
+
+            gt = double(squeeze(ValidationGtBlocksSrgb(i,b,:,:,:)));
+            denoised = double(DenoisedBlocksSrgb{i,b}); 
+
+            %calc SSIM
+
+             rSSIM = ssim(denoised(:,:,1), gt(:,:,1)); 
+             gSSIM = ssim(denoised(:,:,2), gt(:,:,2)); 
+             bSSIM = ssim(denoised(:,:,3), gt(:,:,3)); 
+      
+             SSIMs{i,b} = mean([rSSIM, gSSIM, bSSIM]); 
         end 
+        avg_ssim(j,i) = mean([SSIMs{i,:}]); %avg psnr for current image
+        
     end 
-    
+
+    j = j+1;  %update counter for methods
+
+    fprintf('done!\n')
+ 
 end 
+
+%save results 
+filename = 'ssim_results.mat'; 
+
+save(filename, "avg_ssim"); 
