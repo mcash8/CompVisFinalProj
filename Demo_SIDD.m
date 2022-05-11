5a35633000b16658212dceb5ac08d24e1c0ee2c6
Martha Cash <63068410+mcash8@users.noreply.github.com>
2022-05-11 15:26:23 -0500

    Update Demo_SIDD.m

--- Demo_SIDD.m
+++ Demo_SIDD.m
@@ -14,7 +14,8 @@
 % Note: change this line if the Data is not in the parent directory
 % Note: SIDD_Benchmark_Data can be downloaded from: 
 % https://www.eecs.yorku.ca/~kamel/sidd/benchmark.php
-SiddDataDir = 'C:\Users\mcash8\Downloads\SIDD_Benchmark_Code_v1.1\SIDD_Benchmark_Code_v1.1\SIDD_Benchmark_Data\SIDD_Benchmark_Data'; 
+%SiddDataDir = 'C:\Users\mcash8\Downloads\SIDD_Benchmark_Code_v1.1\SIDD_Benchmark_Code_v1.1\SIDD_Benchmark_Data\SIDD_Benchmark_Data'; 
+SiddDataDir = 'D:\SIDD\ValidationNoisyBlocksSrgb'; 
 
 % path for a Gaussian noise estimatoin method
 % Note: you may replace this mehtod with your favourite one
@@ -32,7 +33,6 @@ OptionalData.MachineSpecs = 'Intel Core i7 6700 @ 3.4 GHz, 32 GB RAM';
 % Note: you may add more optional data as needed
 
 % denoise Raw-RGB images
-%SIDD_Denoise(@DummyDenoiserRaw, SiddDataDir, 'raw', OptionalData);
 Method = input('Enter correction method\n', "s");
 Method = strrep(lower(Method),' ','') ; 
 
@@ -40,12 +40,12 @@ OptionalData.SaveName = [Method,'.mat'];
 
 switch Method
     case 'gamma'
-        SIDD_Denoise(@GammaCorrect, SiddDataDir, 'srgb', OptionalData);
+        SIDD_Denoise(@GammaCorrect, SiddDataDir, 'srgb', OptionalData, Method);
     case 'clahe'
-        SIDD_Denoise(@CLAHECorrect, SiddDataDir, 'srgb', OptionalData);
+        SIDD_Denoise(@CLAHECorrect, SiddDataDir, 'srgb', OptionalData, Method);
     case 'histogramequalization'
-        SIDD_Denoise(@HECorrect, SiddDataDir, 'srgb', OptionalData);
-    case 'CBM3D'
-        print('in progress')
+        SIDD_Denoise(@HECorrect, SiddDataDir, 'srgb', OptionalData, Method);
+    case 'cbm3d'
+      SIDD_Denoise(@CBM3D, SiddDataDir, 'srgb', OptionalData, Method); 
         
 end 
