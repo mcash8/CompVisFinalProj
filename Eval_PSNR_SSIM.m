eeac085282596e8e31d47e95e3e97ca223671e79
Martha Cash <63068410+mcash8@users.noreply.github.com>
2022-05-11 15:43:21 -0500

    Update Eval_PSNR_SSIM.m

--- Eval_PSNR_SSIM.m
+++ Eval_PSNR_SSIM.m
@@ -5,46 +5,46 @@ load(gt_dir, "-mat", 'ValidationGtBlocksSrgb');
 %baseline results DIR 
 baseline_dir = 'D:\SIDD\Results\'; 
 
-methods = {'gamma', 'clahe', 'histogramequalization'}; 
+methods = {'gamma', 'clahe', 'histogramequalization', 'cbm3d'}; 
 
 nImages = 40; 
 nBlocks = 32;
 PSNRs = cell(nImages, nBlocks); %used in for loop
 
-% avg_psnr  = zeros(4, 40); 
-% j=1;
-% for method = methods
-%      Method = string(method); 
-%      baseline_file = strcat(Method, '.mat'); 
-%      
-%      %load .mat file -- results are in DenoisedBlocksSrgb variable
-%      %DenoisedBlockSrgb: 40x32 rows = benchmark images columns = patch 
-%      cd(baseline_dir)
-%      load(baseline_file);         
-%     for i = 1:nImages
-%         for b = 1 : nBlocks 
-% 
-%             fprintf('Calculating PSNR on sRGB image %02d, block %02d ... \n ', i, b);
-% 
-%             gt = double(squeeze(ValidationGtBlocksSrgb(i,b,:,:,:)));
-%             denoised = double(DenoisedBlocksSrgb{i,b}); 
-% 
-%             %calc PSNR
-%             PSNRs{i,b} = psnr(denoised, gt); 
-%         end 
-%         avg_psnr(j,i) = mean([PSNRs{i,:}]); %avg psnr for current image
-%         
-%     end 
-% 
-%     j = j+1;  %update counter for methods
-% 
-%     fprintf('done!\n')
-% end 
-% 
-% %save results 
-% filename = 'psnr_results.mat'; 
-% 
-% save(filename, "avg_psnr"); 
+avg_psnr  = zeros(4, 40); 
+j=1;
+for method = methods
+     Method = string(method); 
+     baseline_file = strcat(Method, '.mat'); 
+     
+     %load .mat file -- results are in DenoisedBlocksSrgb variable
+     %DenoisedBlockSrgb: 40x32 rows = benchmark images columns = patch 
+     cd(baseline_dir)
+     load(baseline_file);         
+    for i = 1:nImages
+        for b = 1 : nBlocks 
+
+            fprintf('Calculating PSNR on sRGB image %02d, block %02d ... \n ', i, b);
+
+            gt = double(squeeze(ValidationGtBlocksSrgb(i,b,:,:,:)));
+            denoised = double(DenoisedBlocksSrgb{i,b}); 
+
+            %calc PSNR
+            PSNRs{i,b} = psnr(denoised, gt); 
+        end 
+        avg_psnr(j,i) = mean([PSNRs{i,:}]); %avg psnr for current image
+        
+    end 
+
+    j = j+1;  %update counter for methods
+
+    fprintf('done!\n')
+end 
+
+%save results 
+filename = 'psnr_results.mat'; 
+
+save(filename, "avg_psnr"); 
 
 %calculating mean SSIM
 
@@ -69,7 +69,6 @@ for method = methods
             denoised = double(DenoisedBlocksSrgb{i,b}); 
 
             %calc SSIM
-
              rSSIM = ssim(denoised(:,:,1), gt(:,:,1)); 
              gSSIM = ssim(denoised(:,:,2), gt(:,:,2)); 
              bSSIM = ssim(denoised(:,:,3), gt(:,:,3)); 
